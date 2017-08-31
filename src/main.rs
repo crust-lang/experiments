a570fff5f68f531429629ed37b99715ced7cefd0
Geoff Shannon <geoffpshannon@gmail.com>
2017-08-30 22:41:28 -0500

    Reimplement basic interaction with readline like ability

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
-use std::io::{self, Read, Write};
+extern crate rustyline;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 fn main() {
-    loop {
-        io::stdout().write(b"lispy> ");
-        io::stdout().flush();
-        let mut buffer = String::new();
-        io::stdin().read_line(&mut buffer).unwrap();
-        buffer.pop();
-        println!("No you're a {}", buffer);
+    let mut rl = Editor::<()>::new();
+    if let Err(_) = rl.load_history("history.txt") {
+        println!("No previous history.");
     }
+
+    loop {
+        let readline = rl.readline("lispy> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(&line);
+                println!("No you're a {}", line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
+    }
+    rl.save_history("history.txt").unwrap();
 }
